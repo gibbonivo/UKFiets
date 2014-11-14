c73b10482d4948d20e3acf2446a631f0cb962f6c
S2455951 <s2455951@fwn-bborg-edu-74-11.fmnsedu.rug.nl>
2014-11-14 12:30:55 +0100

    Added Randy's stuff

---
+++ scripts/entry.js
@@ -0,0 +1,15 @@
+addEventListener("load", function() {
+	logManager = new ENGINE2D.LogManager();
+	loadingManager = new ENGINE2D.LoadingManager(logManager);
+
+	interval = new ENGINE2D.Interval(logManager);
+	renderer = new ENGINE2D.Renderer(logManager);
+	simulator = new UKGame({},loadingManager,logManager);
+
+	game = new ENGINE2D.Game(interval, renderer, simulator, logManager, loadingManager);
+
+	game.SetUp();
+	renderer.FullScreen();
+	game.Start();
+	//game.Interupt();
+});
